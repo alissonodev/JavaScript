fcb774bf455762d5c0c24e433eed834c1ccd77bf
Alisson Cardoso <alisson.fcardoso15@gmail.com>
2025-06-21 22:28:31 -0300

    Atualização do site "hora do dia"

--- exercício01/script.js
+++ exercício01/script.js
@@ -1,1 +1,20 @@
-alert("olá mundo")
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora <= 12){
+        // BOM DIA!
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#FDBA40'
+    } else if (hora >= 12 && hora <= 18) {
+        // BOA TARDE
+        img.src = 'fototarde.png'
+        document.body.style.background = '#024DBA'
+    } else {
+        // BOA NOITE
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#2A2A30'
+    }
+}
