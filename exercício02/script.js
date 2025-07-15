94c8fbfa1d26dba7e054c9455513255af8deda36
Alisson Cardoso <alisson.fcardoso15@gmail.com>
2025-07-15 00:04:50 -0300

    exercício 02: finalizando

--- exercício02/script.js
+++ exercício02/script.js
@@ -1,1 +1,41 @@
-alert("olá mundo")
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || fano.value > ano){
+        alert('[ERRO] Verifique os dados e tente novamente!')
+    }else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero =  ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homen'
+            if(idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'imgbebem.png')
+            }else if (idade < 21) {
+                img.setAttribute('src', 'imgjovemm.png')
+            }else if (idade < 50) {
+                img.setAttribute('src', 'imgadultom.png')
+            }else {
+                img.setAttribute('src', 'imgvelhom.png')
+            } 
+        }else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if(idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'imgbebef.png')
+            }else if (idade < 21) {
+                img.setAttribute('src', 'imgjovemf.png')
+            }else if (idade < 50) {
+                img.setAttribute('src', 'imgadultof.png')
+            }else {
+                img.setAttribute('src', 'imgvelhof.png')
+            } 
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
